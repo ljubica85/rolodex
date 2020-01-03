db1010bdbf7b9010b683bada6830eeceb7a189c4
ljubica <ljubica.aleksic85@gmaill.com>
2020-01-03 16:38:53 +0100

    Updates

---
+++ precache-manifest.3d42e74ef1166930a11392770cbcf226.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c9103874631ab8ed37b1bf4b07bcf6",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "d4913b19ff4b63582dd9",
+    "url": "/rolodex/static/css/main.860da9cc.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d4913b19ff4b63582dd9",
+    "url": "/rolodex/static/js/main.b0ff1836.chunk.js"
+  },
+  {
+    "revision": "d72009913ef41833d3a3",
+    "url": "/rolodex/static/js/runtime-main.2fc8d5fe.js"
+  }
+]);
