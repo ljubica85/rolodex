d6ad42156da57ed3745c952b83a9f2f85f42cc47
ljubica <ljubica.aleksic85@gmaill.com>
2020-09-22 12:10:20 +0200

    changing the design

--- src/components/choice/Choice.js
+++ src/components/choice/Choice.js
@@ -10,16 +10,16 @@ import "./choice.css";
 export const Choice = ({ handleClick }) => (
   <div className="flex">
     <button className="choice-button" onClick={() => handleClick(1)}>
-      <GiVintageRobot size={50} color="#0ccac4" />
+      <GiVintageRobot size={50} />
     </button>
     <button className="choice-button" onClick={() => handleClick(2)}>
-      <GiVampireDracula size={50} color="#0ccac4" />
+      <GiVampireDracula size={50} />
     </button>
     <button className="choice-button" onClick={() => handleClick(3)}>
-      <GiRobotAntennas size={50} color="#0ccac4" />
+      <GiRobotAntennas size={50} />
     </button>
     <button className="choice-button" onClick={() => handleClick(4)}>
-      <GiWhiteCat size={50} color="#0ccac4" />
+      <GiWhiteCat size={50} />
     </button>
   </div>
 );
